df05ce1da4235a94270acc82cbc1e4d0397cbd33
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-05-27 17:25:30 -0300

    feat: implements user service layer

--- src/main/java/com/th/taskproject/services/UserService.java
+++ src/main/java/com/th/taskproject/services/UserService.java
@@ -1,4 +1,40 @@
 package com.th.taskproject.services;
 
+import com.th.taskproject.entities.User;
+import com.th.taskproject.exceptions.ResourceNotFoundException;
+import com.th.taskproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> ListAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User user) {
+        User updatedUser = findUserById(id);
+        updatedUser.setName(user.getName());
+        updatedUser.setEmail(user.getEmail());
+
+        return userRepository.save(updatedUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
