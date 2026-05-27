7e85f066c971b6e57d035303ba6c4ca7e7057a6a
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-05-27 17:24:27 -0300

    feat: implements global exception handling

--- src/main/java/com/th/taskproject/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/th/taskproject/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package com.th.taskproject.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
